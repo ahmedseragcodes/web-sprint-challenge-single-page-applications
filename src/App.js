58ad68d89c1897408c271bd4f5e6b0d2fddc7b68
ahmedseragcodes <ahmedserag1@gmail.com>
2020-11-13 12:35:25 -0500

    added event handler and on submit handler

--- src/App.js
+++ src/App.js
@@ -11,14 +11,33 @@ const App = () => {
   const [form, setForm]=useState({
     size: "",
     sauce: "",
-    toppings: "",
+    toppings1: "",
+    toppings2: "",
+    toppings3: "",
+    toppings4: "",
+    toppings5: "",
+    toppings6: "",
     substitute: "",
     instructions: "",
     quantity: null,
   })
 
   //event handler for form upon change 
-  const handleChange = event => { setForm(event.target.value)};
+  const handleChange = event => {
+    //Pulling out info that matters
+    const { name, type, value, checked }=event.target; 
+    //Determining value to use
+    const updatedInfo= type==="checkbox" ? checked : value;
+    //Updating State 
+    setForm({...form, [name]: updatedInfo })
+    
+  };
+
+  //on submit handler
+  const handleSubmit= event => {
+    event.preventDefault();
+    console.log("submitted");
+  }
 
 
   //return for main component function creating form
@@ -27,18 +46,18 @@ const App = () => {
       <h1>Lambda Eats</h1>
       <h2>Build Your Own Pizza🍕</h2>
       <img src="https://storage.pizzapizza.ca/phx2/ppl_images/category/fr/2x/create_your_own_2.png" alt="picture of pizza" />
-      <form>
+      <form onSubmit={handleSubmit}>
         {/*pizza size dropdown*/}
         <h2>Choose Your Pizza Size</h2>
         <label htmlFor="size">
           <select name="size" value={form.size} onChange={handleChange} >
             <option value="">Select</option>
-            <option value="">Small</option>
-            <option value="">Medium</option>
-            <option value="">Large</option>
+            <option value="Small">Small</option>
+            <option value="Medium">Medium</option>
+            <option value="Large">Large</option>
           </select>
         </label>
-        {/*Sauce Radio Selection */}
+        {/*Sauce Radio Selection*/}
         <h2>Choose Your Pizza Sauce</h2>
         <label htmlFor="sauce">Original Red Sauce
         <input name="sauce" type="radio" value="a" checked={form.sauce==="a"} onChange={handleChange} />
@@ -52,25 +71,25 @@ const App = () => {
         <label htmlFor="sauce">Spinach Alfredo
         <input name="sauce" type="radio" value="d" checked={form.sauce==="d"} onChange={handleChange} />
         </label>
-        {/* Toppings Checkbox */}
+        {/* Toppings Checkbox, 6 Checkboxes */}
         <h2>Add Toppings</h2>
-        <label htmlFor="toppings">Extra Cheese
-          <input name="toppings" type="checkbox" checked={form.toppings} onChange={handleChange} />
+        <label htmlFor="toppings1">Extra Cheese
+          <input name="toppings1" type="checkbox" checked={form.toppings1} onChange={handleChange}  />
         </label>
-        <label htmlFor="toppings">Pepperoni
-          <input name="toppings" type="checkbox" checked={form.toppings} onChange={handleChange} />
+        <label htmlFor="toppings2">Pepperoni
+          <input name="toppings2" type="checkbox" checked={form.toppings2} onChange={handleChange}  />
         </label>
-        <label htmlFor="toppings">Green Pepper
-          <input name="toppings" type="checkbox" checked={form.toppings} onChange={handleChange} />
+        <label htmlFor="toppings3">Green Pepper
+          <input name="toppings3" type="checkbox" checked={form.toppings3} onChange={handleChange}  />
         </label>
-        <label htmlFor="toppings">Onions
-          <input name="toppings" type="checkbox" checked={form.toppings} onChange={handleChange} />
+        <label htmlFor="toppings4">Onions
+          <input name="toppings4" type="checkbox" checked={form.toppings4} onChange={handleChange} />
         </label>
-        <label htmlFor="toppings">Black Olives
-          <input name="toppings" type="checkbox" checked={form.toppings} onChange={handleChange} />
+        <label htmlFor="toppings5">Black Olives
+          <input name="toppings5" type="checkbox" checked={form.toppings5} onChange={handleChange}  />
         </label>
-        <label htmlFor="toppings">Pineapple
-          <input name="toppings" type="checkbox" checked={form.toppings} onChange={handleChange} />
+        <label htmlFor="toppings6">Pineapple
+          <input name="toppings6" type="checkbox" checked={form.toppings6} onChange={handleChange} />
         </label>
         {/*Choice Of Substitute Crust Checkbox*/}
         <h2>Change Your Crust</h2>
@@ -89,7 +108,7 @@ const App = () => {
         </label>
         {/*Add to Order Button*/}
         <h2>Finalize Order</h2>
-        <button type="submit">Add to Order $17.99</button>
+        <button>Add to Order $17.99</button>
       </form>
     </div>
   );
