d3ce88251e16095fa0a8de00777827b37f827945
ahmedseragcodes <ahmedserag1@gmail.com>
2021-01-29 11:55:51 -0500

    setup props and on change event handler

--- src/Components/orderform.js
+++ src/Components/orderform.js
@@ -3,11 +3,29 @@ import { Route, Link } from "react-router-dom";
 import styled from "styled-components";
 import axios from "axios";
 
-export default function OrderForm(){
+export default function OrderForm(props){
+
+
+const { formValues, setFormValues, formErrors, setFormErrors, orders, setOrders, disabled, setDisabled, onSubmit }=props;
+
+    const onChange=function(event){
+
+        const { name, value, type, checked }=event.target;
+
+
+
+    }
 
     return (
-        <div>
-            
-        </div>
+        <PizzaOrder>
+
+        </PizzaOrder>
     )
 }
+
+const PizzaOrder=styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+font-size: 1.8rem;
+`
