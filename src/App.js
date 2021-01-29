7b35ea3ce119d3dda7516592b8f0b8753c4e10d6
ahmedseragcodes <ahmedserag1@gmail.com>
2021-01-29 11:34:19 -0500

    created required slices of state and on submit event handler

--- src/App.js
+++ src/App.js
@@ -1,16 +1,71 @@
-import React, { useState } from "react";
-import "../src/app.css";
+import React, { useState, useEffect } from "react";
+import "../src/App.css";
 import { Route, Link } from "react-router-dom";
 import styled from "styled-components";
 import axios from "axios";
 import OrderForm from "../src/Components/orderform";
 
+
+
+
 const App = () => {
+
+const initialFormValues={
+  pizzaSize: "",
+  pizzaSauce: "",
+  toppingsPep: false,
+  toppingsExtraCheese: false,
+  toppingsOlives: false,
+  toppingsOnions: false,
+  pizzaInstructions: ""
+}
+
+const initialFormErrors={
+  pizzaSize: "",
+  pizzaSauce: "",
+  toppingsPep: "",
+  toppingsExtraCheese: "",
+  toppingsOlives: "",
+  toppingsOnions: "",
+  pizzaInstructions: ""
+}
+
+const initialOrders=[];
+const initialDisabled=true;
+
+
+  const [formValues, setFormValues]=useState(initialFormValues);
+  const [formErrors, setFormErrors]=useState(initialFormErrors);
+  const [orders, setOrders]=useState(initialOrders);
+  const [disabled, setDisabled]=useState(initialDisabled);
+
+  const onSubmit=function(event){
+    event.preventDefault();
+
+    const newOrder={
+      pizzaSize: formValues.pizzaSize,
+      pizzaSauce: formValues.pizzaSauce,
+      toppingsPep: formValues.toppingsPep,
+      toppingsExtraCheese: formValues.toppingsExtraCheese,
+      toppingsOlives: formValues.toppingsOlives,
+      toppingsOnions: formValues.toppingsOnions,
+      pizzaInstructions: formValues.pizzaInstructions
+    }
+
+    setOrders([...orders, newOrder]);
+    setFormValues(initialFormValues);
+
+  }
+
+
   return (
-    <>
+    <div>
+      <HomeHeading>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <
+      </HomeHeading>
+    </div>
   );
 };
 export default App;
+
